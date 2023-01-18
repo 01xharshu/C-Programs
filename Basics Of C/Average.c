cfa8a5ce12fc4f75beb35000fa520c618ecbdde4
Harsh Mishra <108367606+01xharshu@users.noreply.github.com>
2023-01-18 19:58:47 +0530

    C

---
+++ Basics Of C/Average.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int n,i,a,b=0;
+    printf("\nAverage of n numbers\nEnter value of n:");
+    scanf("%d",&n);
+    for(i=0;i<=n;i++)
+    {
+        printf("\nEnter a number:");
+        scanf("%d",&a);
+        b+=a;
+    }
+    printf("Average =%d",b/n);
+}
