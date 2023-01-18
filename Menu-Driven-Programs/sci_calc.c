6a271d628872b28223be1a424b466d48b06c9829
Harsh Mishra <108367606+01xharshu@users.noreply.github.com>
2023-01-18 20:13:42 +0530

    c

---
+++ Menu-Driven-Programs/sci_calc.c
@@ -0,0 +1,9 @@
+//Scientific Calculator in C using Loops
+#include<stdio.h>
+void main()
+{
+    int n;
+    while(n<0){
+    printf("Welcome user!\nEnter Your Choice:\n1 for Addition\n2 for Substraction\n3 for multiplication\n4 for Division\n5 for Modulus\n6 for Root of a Number\n7 for Additive Inverse\n8 for Multiplicative Inverse\n0 for Exit");
+    }
+}
