e6575127eee700ae501d489688df3a9c693f0577
Harsh Mishra <108367606+01xharshu@users.noreply.github.com>
2023-01-19 16:44:43 +0530

    code-c

--- Menu-Driven-Programs/sci_calc.c
+++ Menu-Driven-Programs/sci_calc.c
@@ -2,8 +2,16 @@
 #include<stdio.h>
 void main()
 {
-    int n;
-    while(n<0){
-    printf("Welcome user!\nEnter Your Choice:\n1 for Addition\n2 for Substraction\n3 for multiplication\n4 for Division\n5 for Modulus\n6 for Root of a Number\n7 for Additive Inverse\n8 for Multiplicative Inverse\n0 for Exit");
+    int n,m;
+    while(n<0||n>0){
+        printf("Welcome user!\nEnter Your Choice:\n1 for Addition\n2 for Substraction\n3 for multiplication\n4 for Division\n5 for Modulus\n6 for Root of a Number\n7 for Additive Inverse\n8 for Multiplicative Inverse\n0 for Exit");
+        scanf("%d",&n);
+        if(n==1)
+        {
+            //Addition
+            printf("\nEnter total Number to be added:");
+            scanf("")
+        }
     }
+
 }
